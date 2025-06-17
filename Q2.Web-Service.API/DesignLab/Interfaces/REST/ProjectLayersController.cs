49799ebf63cb09116073b91b97072334cfb749b9
JonatanFD <118645187+JonatanFD@users.noreply.github.com>
2025-06-17 10:47:57 -0500

    feat: add ProjectController and ProjectLayersController with basic structure and routing setup

---
+++ Q2.Web-Service.API/DesignLab/Interfaces/REST/ProjectLayersController.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using Q2.Web_Service.API.DesignLab.Domain.Services;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Q2.Web_Service.API.DesignLab.Interfaces.REST;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+[SwaggerTag("Available Project Endpoints")]
+public class ProjectLayersController(
+    IProjectCommandService projectCommandService,
+    IProjectQueryService projectQueryService
+) : ControllerBase
+{
+}
