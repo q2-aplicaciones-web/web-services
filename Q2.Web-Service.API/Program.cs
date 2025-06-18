57ddae1a10d747ca499c39004317b03ba86e4a2f
zGIKS <mateo.aleman.romano@gmail.com>
2025-06-18 17:50:19 -0500

    feat: configure database context and register analytics repositories and query services

--- Q2.Web-Service.API/Program.cs
+++ Q2.Web-Service.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<Q2.Web_Service.API.analytics.infrastructure.persistence.jpa.repositories.CustomerAnalyticsRepository>();
+builder.Services.AddScoped<Q2.WebService.API.Analytics.Application.Internal.QueryServices.CustomerAnalyticsQueryServiceImpl>();
+builder.Services.AddScoped<Q2.Web_Service.API.analytics.infrastructure.persistence.jpa.repositories.ManufacturerAnalyticsRepository>();
+builder.Services.AddScoped<Q2.WebService.API.Analytics.Application.Internal.QueryServices.ManufacturerAnalyticsQueryServiceImpl>();
+builder.Services.AddDbContext<Q2.Web_Service.API.Shared.Infrastructure.Persistence.EFC.Configuration.AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
