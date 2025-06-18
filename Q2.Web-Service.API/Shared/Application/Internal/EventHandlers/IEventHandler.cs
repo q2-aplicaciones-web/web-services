365911271cf52bb52e602231bb404ff4d4787ec9
JonatanFD <118645187+JonatanFD@users.noreply.github.com>
2025-06-18 13:18:35 -0500

    feat: add event and event handler interfaces, implement logging command behavior, and update folder structure for shared infrastructure

---
+++ Q2.Web-Service.API/Shared/Application/Internal/EventHandlers/IEventHandler.cs
@@ -0,0 +1,9 @@
+﻿using Cortex.Mediator.Notifications;
+using Q2.Web_Service.API.Shared.Domain.Model.Events;
+
+namespace Q2.Web_Service.API.Shared.Application.Internal.EventHandlers;
+
+public interface IEventHandler<in TEvent> : INotificationHandler<TEvent> where TEvent : IEvent
+{
+    
+}
---
+++ Q2.Web-Service.API/Shared/Infrastructure/Mediator/Cortex/Configuration/LogginCommandBehavior.cs
@@ -0,0 +1,11 @@
+﻿using Cortex.Mediator.Commands;
+
+namespace Q2.Web_Service.API.Shared.Infrastructure.Mediator.Cortex.Configuration;
+
+public class LogginCommandBehavior<TCommand> : ICommandPipelineBehavior<TCommand> where TCommand : ICommand
+{
+    public async Task Handle(TCommand command, CommandHandlerDelegate next, CancellationToken cancellationToken)
+    {
+        await next();
+    }
+}
