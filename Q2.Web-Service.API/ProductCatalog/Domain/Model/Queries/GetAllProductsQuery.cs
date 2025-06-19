69985d0f042042570e0429071752fd0f5868e863
fabriziocpa <fabriziocpa1516@gmail.com>
2025-06-18 19:09:45 -0500

    feat: add query records for product retrieval including GetAllProductsQuery, GetProductByIdQuery, GetProductsByProjectIdQuery, and SearchProductsByTagsQuery

---
+++ Q2.Web-Service.API/ProductCatalog/Domain/Model/Queries/GetAllProductsQuery.cs
@@ -0,0 +1,7 @@
+namespace Q2.WebService.API.ProductCatalog.Domain.Model.Queries
+{
+    /// <summary>
+    /// Query to get all products
+    /// </summary>
+    public record GetAllProductsQuery();
+}
