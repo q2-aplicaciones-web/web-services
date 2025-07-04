092810f42d1c002838c283cfdd5328bbd13dcc84
Neil <98752609+Neilcuri@users.noreply.github.com>
2025-07-04 18:03:26 -0500

    feat: Implement GetOrderByIdQuery and GetOrdersByUserIdQuery with corresponding query service

---
+++ Q2.Web-Service.API/OrdersProcessing/Application/Internal/QueryServices/OrderProcessingQueryService.cs
@@ -0,0 +1,20 @@
+﻿using Q2.Web_Service.API.OrdersProcessing.Domain.Model.Aggregates;
+using Q2.Web_Service.API.OrdersProcessing.Domain.Model.Queries;
+using Q2.Web_Service.API.OrdersProcessing.Domain.Repositories;
+using Q2.Web_Service.API.OrdersProcessing.Domain.Services;
+
+namespace Q2.Web_Service.API.OrdersProcessing.Application.Internal.QueryServices;
+
+public class OrderProcessingQueryService(IOrderProcessingRepository repository) : IOrderProcessingQueryService
+{
+    public async Task<IEnumerable<OrderProcessing>> Handle(GetOrdersByUserIdQuery query)
+    {
+        return await repository.GetAllOrdersByUserIdAsync(query.UserId);
+    }
+
+    public async Task<OrderProcessing?> Handle(GetOrderByIdQuery query)
+    {
+        return await repository.GetOrderByIdAsync(query.OrderId);
+    }
+}
+
