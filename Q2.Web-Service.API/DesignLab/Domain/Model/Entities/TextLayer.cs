d7ff1bac60823104c4dce72d3e4c6f0ca86c65fe
fabriziocpa <fabriziocpa1516@gmail.com>
2025-07-04 18:49:08 -0500

    feat: Refactor Layer and TextLayer classes for improved EF Core integration; add async update method in ILayerRepository

--- Q2.Web-Service.API/DesignLab/Domain/Model/Entities/TextLayer.cs
+++ Q2.Web-Service.API/DesignLab/Domain/Model/Entities/TextLayer.cs
@@ -12,6 +12,14 @@ public class TextLayer : Layer
     public bool IsItalic { get; set; }
     public bool IsUnderline { get; set; }
 
+    // Constructor protegido requerido por EF Core
+    protected TextLayer() : base()
+    {
+        Text = null!; // Will be set by EF Core
+        FontColor = null!; // Will be set by EF Core
+        FontFamily = null!; // Will be set by EF Core
+    }
+
     public TextLayer(CreateTextLayerCommand command) : base(command)
     {
         Text= command.Text;
@@ -21,5 +29,17 @@ public class TextLayer : Layer
         IsBold = command.IsBold;
         IsItalic = command.IsItalic;
         IsUnderline = command.IsUnderline;
+    }
+
+    public void UpdateDetails(UpdateTextLayerCommand command)
+    {
+        Text = command.Text;
+        FontSize = command.FontSize;
+        FontColor = command.FontColor;
+        FontFamily = command.FontFamily;
+        IsBold = command.IsBold;
+        IsItalic = command.IsItalic;
+        IsUnderline = command.IsUnderlined;
+        // UpdatedAt will be handled by the base Layer class if we add it
     }
 }
