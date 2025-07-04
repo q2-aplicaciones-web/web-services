63667b85f24c0782b5e6bce312bb469fb5ced8ff
fabriziocpa <fabriziocpa1516@gmail.com>
2025-07-04 18:49:14 -0500

    feat: Implement UpdateImageLayerCommand and UpdateTextLayerCommand with properties for image and text layers; add UpdateDetails method in ImageLayer for updating layer attributes

--- Q2.Web-Service.API/DesignLab/Domain/Model/Entities/ImageLayer.cs
+++ Q2.Web-Service.API/DesignLab/Domain/Model/Entities/ImageLayer.cs
@@ -8,10 +8,24 @@ public class ImageLayer : Layer
     public int Width { get; set; }
     public int Height { get; set; }
     
+    // Constructor protegido requerido por EF Core
+    protected ImageLayer() : base()
+    {
+        ImageUrl = null!; // Will be set by EF Core
+    }
+    
     public ImageLayer(CreateImageLayerCommand command) : base(command)
     {
         ImageUrl = new Uri(command.ImageUrl);
         Width = command.Width;
         Height = command.Height;
+    }
+
+    public void UpdateDetails(UpdateImageLayerCommand command)
+    {
+        ImageUrl = new Uri(command.ImageUrl);
+        Width = command.Width;
+        Height = command.Height;
+        // UpdatedAt will be handled by the base Layer class if we add it
     }
 }
