3b8e0d522316983508654e69310d54ba15494d0d
zGIKS <166082577+zGIKS@users.noreply.github.com>
2025-07-04 22:29:28 -0500

    refactor: Remove obsolete value objects and introduce ProductStatus enum for better product state management

--- Q2.Web-Service.API/ProductCatalog/Domain/Model/Queries/SearchProductsByTagsQuery.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Q2.WebService.API.ProductCatalog.Domain.Model.Queries
-{
-    /// <summary>
-    /// Query to search products by tags
-    /// </summary>
-    public record SearchProductsByTagsQuery(List<string> Tags);
-}
