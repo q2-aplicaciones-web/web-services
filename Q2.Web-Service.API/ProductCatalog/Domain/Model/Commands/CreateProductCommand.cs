5c5e595b699a51147cce7295d263ad50a1fd88e5
zGIKS <166082577+zGIKS@users.noreply.github.com>
2025-07-04 23:55:11 -0500

    refactor: Rename ProjectUserId to UserId in CreateProductCommand and CreateProductResource; update related constructors and methods

--- Q2.Web-Service.API/ProductCatalog/Domain/Model/Commands/CreateProductCommand.cs
+++ Q2.Web-Service.API/ProductCatalog/Domain/Model/Commands/CreateProductCommand.cs
@@ -11,9 +11,9 @@ namespace Q2.Web_Service.API.ProductCatalog.Domain.Model.Commands
         public ProductStatus Status { get; }
         public string ProjectTitle { get; }
         public string ProjectPreviewUrl { get; }
-        public Guid ProjectUserId { get; }
+        public Guid UserId { get; }
 
-        public CreateProductCommand(ProjectId projectId, Money price, ProductStatus status, string projectTitle, string projectPreviewUrl, Guid projectUserId)
+        public CreateProductCommand(ProjectId projectId, Money price, ProductStatus status, string projectTitle, string projectPreviewUrl, Guid userId)
         {
             if (projectId == null)
                 throw new ArgumentException("Project ID cannot be null");
@@ -21,15 +21,15 @@ namespace Q2.Web_Service.API.ProductCatalog.Domain.Model.Commands
                 throw new ArgumentException("Price cannot be null");
             if (string.IsNullOrWhiteSpace(projectTitle))
                 throw new ArgumentException("Project title cannot be null or empty");
-            if (projectUserId == Guid.Empty)
-                throw new ArgumentException("Project user ID cannot be null");
+            if (userId == Guid.Empty)
+                throw new ArgumentException("User ID cannot be null");
 
             ProjectId = projectId;
             Price = price;
             Status = status;
             ProjectTitle = projectTitle;
             ProjectPreviewUrl = projectPreviewUrl;
-            ProjectUserId = projectUserId;
+            UserId = userId;
         }
 
         public CreateProductCommand(string projectId, Money price, ProductStatus status, string projectTitle, string projectPreviewUrl, Guid projectUserId)
