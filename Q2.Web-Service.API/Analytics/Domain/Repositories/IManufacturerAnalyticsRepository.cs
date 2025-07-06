258223d1eec5c90215134c2f0c7d0eae772b5421
Neil <98752609+Neilcuri@users.noreply.github.com>
2025-07-05 20:01:17 -0500

    feat: refactor IManufacturerAnalyticsRepository to change FindByUserIdAsync to FindByManufacturerIdAsync

--- Q2.Web-Service.API/Analytics/Domain/Repositories/IManufacturerAnalyticsRepository.cs
+++ Q2.Web-Service.API/Analytics/Domain/Repositories/IManufacturerAnalyticsRepository.cs
@@ -6,7 +6,5 @@ namespace Q2.Web_Service.API.Analytics.Domain.Repositories;
 
 public interface IManufacturerAnalyticsRepository : IBaseRepository<ManufacturerAnalytics>
 {
-    Task<ManufacturerAnalytics> FindByUserIdAsync(Guid userId);
-    
-    
+    Task<ManufacturerAnalytics> FindByManufacturerIdAsync(Guid manufacturerId);
 }
