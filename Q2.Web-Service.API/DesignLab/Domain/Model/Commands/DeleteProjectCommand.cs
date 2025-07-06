e5e810074138bc414b4d19b91fc1720f863ce66f
Neil <98752609+Neilcuri@users.noreply.github.com>
2025-07-05 23:55:26 -0500

    feat: add DeleteProjectCommand and assembler for converting project ID to command

---
+++ Q2.Web-Service.API/DesignLab/Domain/Model/Commands/DeleteProjectCommand.cs
@@ -0,0 +1,5 @@
+using Q2.Web_Service.API.DesignLab.Domain.Model.ValueObjects;
+
+namespace Q2.Web_Service.API.DesignLab.Domain.Model.Commands;
+
+public record DeleteProjectCommand(ProjectId ProjectId);
