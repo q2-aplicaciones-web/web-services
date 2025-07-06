6374ed987e8bc0c6378d62980080d9e03331b88c
Neil <98752609+Neilcuri@users.noreply.github.com>
2025-07-06 03:23:43 -0500

    feat: add UpdateLayerCoordinatesCommand record for layer coordinate updates

---
+++ Q2.Web-Service.API/DesignLab/Domain/Model/Commands/UpdateLayerCoordinatesCommand.cs
@@ -0,0 +1,11 @@
+﻿using Q2.Web_Service.API.DesignLab.Domain.Model.ValueObjects;
+
+namespace Q2.Web_Service.API.DesignLab.Domain.Model.Commands;
+
+public record UpdateLayerCoordinatesCommand(
+    ProjectId ProjectId,
+    LayerId LayerId,
+    int X,
+    int Y,
+    int Z
+);
